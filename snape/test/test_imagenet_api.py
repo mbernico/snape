a85418b3a69e2b23ba588e0961307ab1f3012750
Brad Sliz <bradsliz@gmail.com>
2017-03-10 09:09:21 -0600

    initial commit of test module for image sets

---
+++ snape/test/test_imagenet_api.py
@@ -0,0 +1,13 @@
+
+import unittest
+from snape.imagenet_api import *
+
+
+class TestImagenetApi(unittest.TestCase):
+
+    def get_synset_image_links(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
