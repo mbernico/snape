8494dc5678d894feb739e0b7ecdca125c54dfb2e
Brad Sliz <bradsliz@gmail.com>
2017-03-16 10:34:29 -0500

    converting self.assert statements to assert for nose test conformity

--- snape/test/test_make_image_dataset.py
+++ snape/test/test_make_image_dataset.py
@@ -14,8 +14,8 @@ class TestImageNet(unittest.TestCase):
         try:
             ImageNet(1).sample_synset_links(wnid, n, im_dir)
             n_images = len(os.listdir(im_dir + '/' + wnid))
-            self.assertEqual(n, n_images, "Did not download n images")
-            self.assertIn(wnid, os.listdir(im_dir), "Did not get the requested synset")
+            assert n == n_images, "Did not download n images"
+            assert wnid in os.listdir(im_dir), "Did not get the requested synset"
         except:
             raise
         finally:
@@ -29,7 +29,7 @@ class TestImageNet(unittest.TestCase):
             ImageNet(1).get_images(n, im_dir)
             sub_dir = im_dir + os.listdir(im_dir)[0]
             n_images = len(os.listdir(sub_dir))
-            self.assertEqual(n, n_images, "Did not download n images")
+            assert n == n_images, "Did not download n images"
         except:
             raise
         finally:
@@ -37,16 +37,16 @@ class TestImageNet(unittest.TestCase):
 
     def test_get_ilsvrc_1000_synsets(self):
         synsets = ImageNet.get_ilsvrc_1000_synsets()
-        self.assertEqual(len(synsets), 1000, "ILSVRC page parsed incorrectly")
+        assert len(synsets) == 1000, "ILSVRC page parsed incorrectly"
 
     def test_get_synset_image_links(self):
         wnid = 'n02114855'
         links = ImageNet.get_synset_image_links(wnid)
-        self.assertGreater(len(links), 0, "Did not return any image links")
+        assert len(links) > 0, "Did not return any image links"
 
     def test_retrieve_class_counts(self):
         class_counts = ImageNet.retrieve_class_counts()
-        self.assertIsInstance(class_counts, pd.core.frame.DataFrame, "Class counts not returned in a dataframe")
+        assert isinstance(class_counts, pd.core.frame.DataFrame), "Class counts not returned in a dataframe"
 
 
 class TestImageGrabber(unittest.TestCase):
@@ -57,12 +57,12 @@ class TestImageGrabber(unittest.TestCase):
         ImageGrabber().download_image(good_url, good_im_path)
         good_im_type = imghdr.what(good_im_path)
         os.remove(good_im_path)
-        self.assertIsNotNone(good_im_type)
+        assert good_im_type is not None
         bad_url = "https://mckinleyleather.com/image/130963084.jpg"
         bad_im_path = "no_ducky.jpg"
         ImageGrabber().download_image(bad_url, bad_im_path)
         is_file = os.path.isfile(bad_im_path)
-        self.assertFalse(is_file)
+        assert not is_file
 
     def test_catch_unavailable_image(self):
         good_url = "http://farm4.static.flickr.com/3290/2998414960_01dd35d094.jpg"
@@ -71,9 +71,9 @@ class TestImageGrabber(unittest.TestCase):
         good_img_data = requests.get(good_url)
         stale_img_data = requests.get(stale_url)
         junk_img_data = requests.get(junk_url)
-        self.assertFalse(ImageGrabber.catch_unavailable_image(good_img_data))
-        self.assertTrue(ImageGrabber.catch_unavailable_image(stale_img_data))
-        self.assertTrue(ImageGrabber.catch_unavailable_image(junk_img_data))
+        assert not ImageGrabber.catch_unavailable_image(good_img_data), "The good image tested was found to be bad"
+        assert ImageGrabber.catch_unavailable_image(stale_img_data), "The stale image tested was found to be good"
+        assert ImageGrabber.catch_unavailable_image(junk_img_data),  "The junk image tested was found to be good"
 
 
 class TestOpenImages(unittest.TestCase):
